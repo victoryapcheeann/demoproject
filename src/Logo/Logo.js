4658bbeca05dcf8a97ede6c7b99d9430b1753a42
Victor Yap <victoryapcheeann@gmail.com>
2018-08-22 20:33:57 +0800

    Added Logo

---
+++ src/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './LogoAnimation';
+
+class App extends Component {
+   componentDidMount() {
+   }
+
+  render() {
+    return (
+        <div>
+            <canvas id="canvasOne" width="100" height="80" ></canvas>
+        </div>
+    );
+  }
+}
+
+export default App;
