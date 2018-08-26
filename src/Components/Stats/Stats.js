f0372500266b2ed67638ce6ff6e5e487ce64d6e7
Victor Yap <victoryapcheeann@gmail.com>
2018-08-26 18:49:04 +0800

    Added chemistry Score

--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -3,8 +3,33 @@ import './Stats.css';
 import { connect } from "react-redux";
 import RadarChart from "./RadarChart/RadarChart";
 import Chord from "./Chord/Chord";
+import _ from 'lodash';
 
 class Stats extends Component {
+    getChemistryScore() {
+      let chemistryScore = 0;
+
+      if (this.props.players.length > 0) {
+          let currentTeam = this.props.players.map(function(value){
+              return value.ID
+          });  
+
+          let chemistryList = this.props.players.map(function(value){
+              return _(value.Chemistry).keyBy('ID').at(currentTeam).value();
+          });  
+
+          chemistryList.map(function(value){
+              return value.map(function(nested){
+                  chemistryScore = chemistryScore + nested.ChemistryPoint
+              })
+          }); 
+          return(chemistryScore)
+      }
+      else {
+          return("Add Player Please")
+      }
+  }
+
   render() {
     return (
       <div>
@@ -16,7 +41,11 @@ class Stats extends Component {
         </div>
         <div className="mainChartContainer rowStyle">
             <div className="chartContainer2 containerShadow">
-                <span className="chartHeader">Team Chemistry</span>
+                <div className="columnStyle">
+                  <span className="chartHeader">Team Chemistry</span>
+                  <br/>
+                  <span className="chartHeader">Score: {this.getChemistryScore()}</span>
+                </div>
                 <Chord players={this.props.players}/>
             </div>
         </div>
